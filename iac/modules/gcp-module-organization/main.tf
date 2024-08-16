f691f4fa3834e2ee8ceb23bc7483e4cde9a0d131
X-I-A <develop@x-i-a.com>
2024-08-16 16:21:18 +0200

    feat: Cosmos level prepare support

--- iac/modules/gcp-module-organization/main.tf
+++ iac/modules/gcp-module-organization/main.tf
@@ -11,3 +11,12 @@ locals {
   settings = lookup(local.landscape, "settings", {})
   cosmos_name = local.settings["cosmos_name"]
 }
+
+data "google_organization" "my_org" {
+  domain = "x-i-a.com"
+}
+
+resource "google_folder" "my_folder" {
+  display_name = "TF Folder Test"
+  parent       = "organizations/${data.google_organization.my_org.org_id}"
+}
