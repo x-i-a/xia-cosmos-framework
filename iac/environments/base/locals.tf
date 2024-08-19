c7a4c8c4f9b7e9961df5def10e017da3c1e7b296
X-I-A <develop@x-i-a.com>
2024-08-19 21:41:12 +0200

    feat: cosmos level rework

--- iac/environments/base/locals.tf
+++ iac/environments/base/locals.tf
@@ -1,6 +1,2 @@
 locals {
-  landscape = yamldecode(file(var.landscape_file))
-  applications = yamldecode(file(var.applications_file))
-  modules = yamldecode(file(var.modules_file))
-  environment_dict = lookup(local.landscape, "environments", {})
 }
