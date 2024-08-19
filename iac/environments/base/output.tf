2db93f33fd5cb2abd1dc5420f5c733c6dee2ef99
X-I-A <develop@x-i-a.com>
2024-08-19 21:58:59 +0200

    feat: cosmos level rework

---
+++ iac/environments/base/output.tf
@@ -0,0 +1,3 @@
+output "all_foundations" {
+  value = local.all_foundations
+}
--- iac/environments/base/variables.tf
+++ iac/environments/base/variables.tf
@@ -4,20 +4,8 @@ variable "landscape_file" {
   default = "../../../config/landscape.yaml"
 }
 
-variable "applications_file" {
-  type = string
-  description = "Application config file"
-  default = "../../../config/applications.yaml"
-}
-
 variable "modules_file" {
   type = string
   description = "Module config file"
   default = "../../../config/modules.yaml"
-}
-
-variable "project_file" {
-  type = string
-  description = "Module config file"
-  default = "../../../config/gcp-project.yaml"
 }
