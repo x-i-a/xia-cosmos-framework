ee8e5ac8a7a955b234ba4e28dfe85265e24c7e09
X-I-A <develop@x-i-a.com>
2024-08-20 22:49:53 +0200

    feat: cosmos level rework

--- iac/modules/gcp-module-organization/main.tf
+++ iac/modules/gcp-module-organization/main.tf
@@ -10,6 +10,17 @@ data "google_organization" "cosmos_org" {
   domain = local.cosmos_org
 }
 
+locals {
+  landscape = var.landscape
+  settings = lookup(local.landscape, "settings", {})
+  cosmos_org = local.settings["cosmos_org"]
+  cosmos_name = local.settings["cosmos_name"]
+  cosmos_bucket = lookup(local.settings, "cosmos_bucket", local.cosmos_name)
+  cosmos_project = local.settings["cosmos_project"]
+  structure = local.landscape["structure"]
+  github_owner = lookup(local.settings, "github_owner", "")
+}
+
 resource "google_project_service" "service_usage_api" {
   project = local.cosmos_project
   service = "serviceusage.googleapis.com"
@@ -47,7 +58,7 @@ resource "google_folder" "realm_l3_folders" {
 }
 
 resource "google_folder" "foundation_folders" {
-  for_each = local.all_foundations
+  for_each = var.foundations
   display_name = each.value.name
   parent       = coalesce(
     lookup(lookup(google_folder.realm_l1_folders, each.value.parent, {}), "name", ""),
@@ -58,29 +69,29 @@ resource "google_folder" "foundation_folders" {
 }
 
 resource "google_service_account" "foundation_admin_sa" {
-  for_each = local.all_foundations
+  for_each = var.foundations
   project      = local.cosmos_project
-  account_id   = "dir-${each.value.name}-sa"
+  account_id   = "dir-${each.value["name"]}-sa"
   display_name = "Service Account for Foundation Directory ${each.value["name"]}"
   depends_on = [google_folder.foundation_folders]
 }
 
 resource "google_folder_iam_member" "foundation_admin_sa_owner" {
-  for_each = local.all_foundations
+  for_each = var.foundations
   folder = google_folder.foundation_folders[each.key].name
   role = "roles/owner"
   member = "serviceAccount:${google_service_account.foundation_admin_sa[each.key].email}"
 }
 
 resource "google_iam_workload_identity_pool" "github_pool" {
-  for_each = local.all_foundations
+  for_each = var.foundations
 
-  workload_identity_pool_id = "wip-${each.value.name}"
+  workload_identity_pool_id = "wip-${each.value["name"]}"
   project  = local.cosmos_project
 
   # Workload Identity Pool configuration
-  display_name = "wip-${each.value.name}"
-  description  = "Pool for GitHub Actions of ${each.value.name}"
+  display_name = "wip-${each.value["name"]}"
+  description  = "Pool for GitHub Actions of ${each.value["name"]}"
 
   # Make sure the pool is in a state to be used
   disabled = false
@@ -89,18 +100,18 @@ resource "google_iam_workload_identity_pool" "github_pool" {
 }
 
 resource "google_iam_workload_identity_pool_provider" "github_provider" {
-  for_each = local.all_foundations
+  for_each = var.foundations
 
   workload_identity_pool_id = google_iam_workload_identity_pool.github_pool[each.key].workload_identity_pool_id
-  workload_identity_pool_provider_id     = "ghp-${each.value.name}"
+  workload_identity_pool_provider_id     = "ghp-${each.value["name"]}"
   project  = local.cosmos_project
 
   # Provider configuration specific to GitHub
-  display_name = "ghp-${each.value.name}"
-  description  = "Provider for GitHub Actions of ${each.value.name}"
+  display_name = "ghp-${each.value["name"]}"
+  description  = "Provider for GitHub Actions of ${each.value["name"]}"
 
    # Attribute mapping / condition from the OIDC token to Google Cloud attributes
-  attribute_condition = "assertion.sub == 'repo:${local.github_owner}/${each.value.repository}' && assertion.ref.matches('main')"
+  attribute_condition = "assertion.sub == 'repo:${local.github_owner}/${each.value["repository_name"]}' && assertion.ref.matches('main')"
 
   attribute_mapping = {
     "google.subject" = "assertion.sub",
@@ -117,7 +128,7 @@ resource "google_iam_workload_identity_pool_provider" "github_provider" {
 }
 
 resource "google_storage_bucket_iam_member" "tfstate_bucket_list" {
-  for_each = local.all_foundations
+  for_each = var.foundations
   bucket = local.cosmos_bucket
   role   = "roles/storage.objectViewer"
   member = "serviceAccount:${google_service_account.foundation_admin_sa[each.key].email}"
